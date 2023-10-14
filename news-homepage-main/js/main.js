62068fa977c99f183adf1f1676ffd0f5d9a7550f
Zyrean <stephanwinzer1990@gmail.com>
2023-10-14 22:50:32 +0200

    Html/Css update + Menu header logic

---
+++ news-homepage-main/js/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const btnMenuOpenPhone = document.querySelector(".img-menu");
+const btnMenuClosePhone = document.querySelector(".img-menu-close");
+const menuPhone = document.querySelector(".container-popup-phone");
+
+const displayMenuPhone = function () {
+  menuPhone.classList.remove("hidden");
+};
+
+const hideMenuPhone = function () {
+  menuPhone.classList.add("hidden");
+};
+
+btnMenuOpenPhone.addEventListener("click", function () {
+  displayMenuPhone();
+  console.log("test");
+});
+
+btnMenuClosePhone.addEventListener("click", function () {
+  hideMenuPhone();
+});
